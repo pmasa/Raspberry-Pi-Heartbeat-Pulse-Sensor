a7437a04d8fc66eb8cd2855b83429d2301311983
pmasa <masamuna.pedro@gmail.com>
2020-04-25 17:36:41 +0200

    first commit

--- MCP3008.py
+++ MCP3008.py
@@ -21,6 +21,7 @@ class MCP3008:
         self.spi.open(self.bus, self.device)
     
     def read(self, channel = 0):
+        self.spi.max_speed_hz = 1300000
         adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
         data = ((adc[1] & 3) << 8) + adc[2]
         return data
--- example.py
+++ example.py
@@ -7,10 +7,12 @@ p.startAsyncBPM()
 try:
     while True:
         bpm = p.BPM
-        if bpm > 0:
-            print("BPM: %d" % bpm)
+        #if bpm > 0:
+        if bpm > 220 and bpm < 235:
+            print("BPM: %d" % (bpm-170))
         else:
-            print("No Heartbeat found")
+            #print("No Heartbeat found")
+            print("BPM: 0")
         time.sleep(1)
 except:
     p.stopAsyncBPM()
